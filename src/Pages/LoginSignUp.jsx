f1a54c058a2b6526e1a523febb5a000e30d57195
themanjeetkr <themanjeetkr@gmail.com>
2025-07-14 15:56:26 +0530

    changes in  app.jsx

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../CSS/LoginSignUp.css'
 
 const LoginSignUp = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container"></div>
+      <h1> Sign Up</h1>
+      <div className="loginsignup-field">
+        <input type="text"  placeholder='Your Name'/>
+        <input type='email' placeholder='Email Address'/>
+        <input type='password' placeholder='Password'></input>
+      </div>
+      <button>Continue</button>
+      <p className="loginsignup-login">
+        Already have an account ? <span>Login Here</span>
+      </p>
+      <div className="loginsignup-agree">
+        <input type="checkbox" name='' id='' />
+        <p>By continuing , i agree to the terms of use & privacy policy</p>
+      </div>
+
     </div>
   )
 }
