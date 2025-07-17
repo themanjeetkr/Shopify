a20687a6490f5068fad408d7f8f6494949c3b0fc
themanjeetkr <themanjeetkr@gmail.com>
2025-07-17 11:57:09 +0530

    Descripton css add

--- src/Components/Description/Description.jsx
+++ src/Components/Description/Description.jsx
@@ -9,13 +9,13 @@ const Description = () => {
             <div className="descriptionbox-nav-box">
                 Description 
             </div>
-            <div className="description-new-box fade">
+            <div className="description-new-box-fade">
                 Review(135)
 
             </div>
            
         </div>
-         <div className="description-description">
+         <div className="descriptionbox-description">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam inventore ab ad pariatur necessitatibus quam voluptatum voluptatem sed vel dolorum, fuga suscipit enim praesentium ipsum repudiandae laborum cupiditate eos? Illum!
                 Asperiores voluptates fugit alias mollitia dolor deleniti, omnis blanditiis, iure nihil modi corporis aut ad aliquam magni sint exercitationem doloremque quisquam pariatur, consectetur impedit! Earum cum architecto provident qui magnam!
                 Nulla explicabo dicta porro ut aut id! A, aperiam facere, deserunt de numquam nulla sequi, ipsum libero sapiente aliquam cum quaerat rerum magnam aliquid pariatur obcaecati modi aperiam molestiae! Deserunt!</p>
